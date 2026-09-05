4b36d30c189e2254e1c90daa542f3242922cc4f8
Saurabh Lokhande <saurabhlokhande664@gmail.com>
2026-09-06 00:15:05 +0530

    Time: 20 ms (12.59%), Space: 14.2 MB (73.72%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,9 +1,36 @@
 class Solution(object):
     def sortedSquares(self, nums):
-        sq=[]
+        
+        neg = []
+        pos = []
+        res = []
+        # Separating negative and positive
         for num in nums:
-            sq.append(num*num)
-            sq.sort()
-        return sq
+            if num < 0:
+                neg.append(num * num)
+            else:
+                pos.append(num * num)
+        # Negative squares are currently in decreasing order
+        neg.reverse()
+        i = 0
+        j = 0
+        # Merge two sorted arrays
+        while i < len(neg) and j < len(pos):
+
+            if neg[i] < pos[j]:
+                res.append(neg[i])
+                i += 1
+            else:
+                res.append(pos[j])
+                j += 1
+        # Remaining positive squares
+        while j < len(pos):
+            res.append(pos[j])
+            j += 1
+        # Remaining negative squares
+        while i < len(neg):
+            res.append(neg[i])
+            i += 1
+        return res
             
         
