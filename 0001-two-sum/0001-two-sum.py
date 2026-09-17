d838659653544d1f4eaa770bb4f77633077624a8
Saurabh Lokhande <saurabhlokhande664@gmail.com>
2026-09-17 22:39:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for left in range(len(nums)):
-            for right in range(left+1,len(nums)):
-                if nums[left]+nums[right]==target:
-                    return [left,right]            
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         
